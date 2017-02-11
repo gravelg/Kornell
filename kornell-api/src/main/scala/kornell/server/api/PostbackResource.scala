3c0998b532cc7d8a96f7b1fbfa668c290352c45c
Gabriel Gravel <gravel.gabriel@gmail.com>
2017-02-11 01:14:18 -0500

    Initial PagSeguro work

--- kornell-api/src/main/scala/kornell/server/api/PostbackResource.scala
+++ kornell-api/src/main/scala/kornell/server/api/PostbackResource.scala
@@ -4,6 +4,7 @@ import javax.ws.rs.Path
 import javax.ws.rs.PathParam
 import javax.ws.rs.POST
 import kornell.server.repository.service.PostbackService
+import javax.ws.rs.FormParam
 
 @Path("/postback")
 class PostbackResource {
@@ -14,6 +15,16 @@ class PostbackResource {
     //This call needs to return a 200 with empty response immediately
     //Service launches thread for rest of processing
     PostbackService.paypalPostback(env, payload)
+    ""
+  }
+  
+  @Path("/pagseguro/{institutionUUID}/{env}")
+  @POST
+  def pagseguro(@PathParam("env") env: String, @PathParam("institutionUUID") institutionUUID: String,
+      @FormParam("notificationCode") notificationCode: String, @FormParam("notificationType") notificationType: String) = {
+    //This call needs to return a 200 with empty response immediately
+    //Service launches thread for rest of processing
+    PostbackService.pagseguroPostback(env, institutionUUID, notificationCode)
     ""
   }
 }
