7dc043ad6657a8575e8df4176c8ec43915d3d450
Tiago Faria <tcfaria@gmail.com>
2017-02-24 17:56:32 -0500

    allow S3 access key and secret to be put as env variables

--- kornell-api/src/main/scala/kornell/server/util/Settings.scala
+++ kornell-api/src/main/scala/kornell/server/util/Settings.scala
@@ -85,6 +85,8 @@ object Settings extends Enum {
   val JDBC_PASSWORD = settting("JDBC_PASSWORD")
   val JDBC_DRIVER =   settting("JDBC_DRIVER","com.mysql.jdbc.Driver")
   val USER_CONTENT_BUCKET = settting("USER_CONTENT_BUCKET","us-east-1.usercontent-develop")
+  val USER_CONTENT_ACCESS_KEY = settting("USER_CONTENT_ACCESS_KEY")
+  val USER_CONTENT_SECRET_ACCESS_KEY = settting("USER_CONTENT_SECRET_ACCESS_KEY")
   val USER_CONTENT_REGION = settting("USER_CONTENT_REGION","us-east-1")
   val TEST_MODE = settting("TEST_MODE")
   val SMTP_HOST = settting("SMTP_HOST")
