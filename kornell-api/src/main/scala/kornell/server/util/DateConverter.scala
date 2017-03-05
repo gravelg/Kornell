cc867854f91e7e258a35eb3750741d67fdf52efb
Tiago Faria <tcfaria@gmail.com>
2017-03-05 15:45:52 -0500

    change DateConverter to try to catch cases where the courseclassuuid is null

--- kornell-api/src/main/scala/kornell/server/util/DateConverter.scala
+++ kornell-api/src/main/scala/kornell/server/util/DateConverter.scala
@@ -6,17 +6,21 @@ import kornell.server.jdbc.repository.PeopleRepo
 import kornell.server.jdbc.repository.InstitutionRepo
 import kornell.server.jdbc.repository.CourseClassRepo
 
-class DateConverter(personUUID: String) {    
+class DateConverter(personUUID: String) {
   def dateToInstitutionTimezone(date: Date) = {
     Option(date) match {
       case Some(s) => {
-        val st = s.getTime
-        new Date(st - DateTimeZone.forID(PeopleRepo.getTimezoneByUUID(personUUID).get).getStandardOffset(st))
+        if (personUUID != null) {
+          val st = s.getTime
+          new Date(st - DateTimeZone.forID(PeopleRepo.getTimezoneByUUID(personUUID).get).getStandardOffset(st))
+        } else {
+          date
+        }
       }
       case None => null
     }
   }
-  
+
   def dateToInstitutionTimezone(date: Date, institutionUUID: String) = {
     Option(date) match {
       case Some(s) => {
@@ -26,12 +30,12 @@ class DateConverter(personUUID: String) {
       case None => null
     }
   }
-  
-   def dateToInstitutionTimezoneFromCourseClass(date: Date, courseClassUUID: String) = {
+
+  def dateToInstitutionTimezoneFromCourseClass(date: Date, courseClassUUID: String) = {
     Option(date) match {
       case Some(s) => {
         val st = s.getTime
-        if(courseClassUUID != null) { 
+        if (courseClassUUID != null) {
           val institutionUUID = CourseClassRepo(courseClassUUID).get.getInstitutionUUID
           new Date(st - DateTimeZone.forID(InstitutionRepo(institutionUUID).get.getTimeZone).getStandardOffset(st))
         } else {
@@ -41,10 +45,10 @@ class DateConverter(personUUID: String) {
       case None => null
     }
   }
-  
+
 }
 
-object DateConverter {  	
+object DateConverter {
   def apply() = new DateConverter(null)
   def apply(personUUID: String) = new DateConverter(personUUID)
 }
