1164107fb398c4b45cbba2bd44d087a8faad9b67
Gabriel Gravel <gravel.gabriel@gmail.com>
2017-03-27 23:51:56 -0400

    Add permissions on endpoint to reprocfess pagseguro xml

--- kornell-api/src/main/scala/kornell/server/api/PostbackResource.scala
+++ kornell-api/src/main/scala/kornell/server/api/PostbackResource.scala
@@ -5,6 +5,9 @@ import javax.ws.rs.PathParam
 import javax.ws.rs.POST
 import kornell.server.service.PostbackService
 import javax.ws.rs.FormParam
+import kornell.server.util.Conditional.toConditional
+import kornell.server.util.AccessDeniedErr
+
 
 @Path("/postback")
 class PostbackResource {
@@ -21,14 +24,17 @@ class PostbackResource {
   @Path("/pagseguro/{institutionUUID}/{env}")
   @POST
   def pagseguro(@PathParam("env") env: String, @PathParam("institutionUUID") institutionUUID: String,
-      @FormParam("notificationCode") notificationCode: String, @FormParam("notificationType") notificationType: String, payload: String) = {
+      @FormParam("notificationCode") notificationCode: String, @FormParam("notificationType") notificationType: String) = {
     //This call needs to return a 200 with empty response immediately
     //Service launches thread for rest of processing
-    if (env == "xml") {
-      PostbackService.processPagseguroResponse(institutionUUID, payload)
-    } else {
-      PostbackService.pagseguroPostback(env, institutionUUID, notificationCode)
-    }
+    PostbackService.pagseguroPostback(env, institutionUUID, notificationCode)
     ""
   }
+  
+  @Path("/reprocess/pagseguro/{institutionUUID}")
+  @POST
+  def reprocessPagseguro(@PathParam("institutionUUID") institutionUUID: String, payload: String) = {
+    PostbackService.processPagseguroResponse(institutionUUID, payload)
+    ""
+  }.requiring(isPlatformAdmin(institutionUUID), AccessDeniedErr()).get
 }
