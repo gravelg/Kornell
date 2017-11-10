082e81f2a9fd12ea6eeecdedce83c8f32dd4c560
Gabriel Gravel <gravel.gabriel@gmail.com>
2017-11-10 00:34:37 -0500

    Unzip files when uploading to FS repo

--- kornell-api/src/main/scala/kornell/server/content/UploaderResource.scala
+++ kornell-api/src/main/scala/kornell/server/content/UploaderResource.scala
@@ -13,6 +13,11 @@ import javax.ws.rs.PUT
 import java.nio.file.Paths
 import kornell.server.jdbc.repository.ContentRepositoriesRepo
 import java.nio.file.StandardCopyOption
+import java.io.File
+import java.io.InputStream
+import java.util.zip.ZipInputStream
+import java.io.ByteArrayOutputStream
+import java.io.ByteArrayInputStream
 
 @Path("/")
 class UploaderResource {
@@ -33,14 +38,48 @@ class UploaderResource {
     destinationPath.getParent.toFile().mkdirs()
 
     val fileContent = req.getInputStream
+    val contentType = req.getHeader("Content-Type")
     try {
-      Files.copy(fileContent, destinationPath, StandardCopyOption.REPLACE_EXISTING)
+      if (contentType == "application/zip") {
+        unzip(destinationPath.getParent.toFile, fileContent)
+      } else {
+        Files.copy(fileContent, destinationPath, StandardCopyOption.REPLACE_EXISTING)
+      }
       Response.status(200).build()
-    } catch {
-      case e: Exception => Response.status(500).entity(s"Key [${path}] not loaded").build()
+//    } catch {
+//      case e: Exception => Response.status(500).entity(s"Key [${path}] not loaded").build()
     } finally {
       fileContent.close()
     }
+  }
+
+  def unzip(uploadDirectory: File, zipContent: InputStream) = {
+    val zis = new ZipInputStream(zipContent)
+    var entry = zis.getNextEntry()
+    val buffer = new Array[Byte](1024)
+
+    while(entry != null) {
+        val fileName = entry.getName()
+        val outputStream = new ByteArrayOutputStream()
+        var len = zis.read(buffer)
+        while (len > 0) {
+            outputStream.write(buffer, 0, len)
+            len = zis.read(buffer)
+        }
+        val is = new ByteArrayInputStream(outputStream.toByteArray())
+        val path = Paths.get(uploadDirectory.getAbsolutePath, fileName)
+        // sometimes zips contain directory entries, don't create because we create on file copy
+        if (path.getFileName.toString.indexOf(".") != -1) {
+          Files.createDirectories(path.getParent)
+          Files.copy(is, path, StandardCopyOption.REPLACE_EXISTING)
+        }
+
+        is.close();
+        outputStream.close();
+        entry = zis.getNextEntry();
+    }
+    zis.closeEntry();
+    zis.close();
   }
 
 }
