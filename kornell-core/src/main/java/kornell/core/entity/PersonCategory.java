696697c25dd187341a764f088f0c912e3e40eade
Gabriel Gravel <gravel.gabriel@gmail.com>
2017-11-22 23:34:11 -0500

    Fix equals bug in PersonCategory

--- kornell-core/src/main/java/kornell/core/entity/PersonCategory.java
+++ kornell-core/src/main/java/kornell/core/entity/PersonCategory.java
@@ -5,7 +5,7 @@ package kornell.core.entity;
 public class PersonCategory {
 
 	public static String getSexSuffix(Person person, String locale) {
-		if (locale == "pt_BR") {
+		if (locale.equals("pt_BR")) {
 			if ("F".equals(person.getSex()))
 				return "a";
 			else if("M".equals(person.getSex()))
