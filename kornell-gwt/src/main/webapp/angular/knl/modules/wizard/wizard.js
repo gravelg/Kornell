111d5cbda7b54c0c69948c1013bd41718b3c5172
Tiago Faria <tcfaria@gmail.com>
2018-03-14 01:43:09 -0400

    save base64 encoded json

--- kornell-gwt/src/main/webapp/angular/knl/modules/wizard/wizard.js
+++ kornell-gwt/src/main/webapp/angular/knl/modules/wizard/wizard.js
@@ -18,7 +18,7 @@ app.controller('WizardController', [
 
     window.addEventListener('message',function(event) {
       if(event.data.type === 'classroomJsonLoad'){
-        $scope.root = JSON.parse(event.data.message);
+        $scope.root = JSON.parse(decodeURI(Base64.decode(event.data.message)));
         $scope.initWizard();
       } else if(event.data.type === 'classroomJsonNew') {
         $scope.newTree(event.data.message);
