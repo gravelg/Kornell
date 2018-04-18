501a52683dbf652d74cfb8020280b5eefe3fa29c
Tiago Faria <tcfaria@gmail.com>
2018-04-18 01:04:58 -0400

    KNL-636 Error while creating a course with a code that exists in another institution

--- kornell-api/src/main/resources/db/migration/2018/04/V2018.04.17.18.05__course_code_constraint.sql
+++ kornell-api/src/main/resources/db/migration/2018/04/V2018.04.17.18.05__course_code_constraint.sql
@@ -1,1 +1,10 @@
-alter table Course drop index code;
+SET @sql = (SELECT IF(
+    (select count(*) from information_schema.table_constraints 
+		where table_schema = 'ebdb'
+		and table_name = 'Course'
+		and constraint_name = 'code'
+    ) = 0,
+    "SELECT 0",
+    "alter table Course drop index code;"
+));
+PREPARE stmt FROM @sql;
